42848be2b88d36d7955b2465e0fbc2a21472fd74
Randy Li <randy.li@rock-chips.com>
2016-07-19 11:46:50 +0800

    librkvpu: encoder: vp8: use the common encoder prototype
    There, there. I have to make you work with Mr.H264. So I make
    the rk_venc adopt you, I am sorry to arrage you a stepfather,
    but it seems you have some prepare for it, as you have used
    a few family(struct) from your dad.
    
    As multiple encoding format have been imported, it is necessary
    to have a common operation standard. Also this part code have not
    been verified.
    
    Signed-off-by: Randy Li <randy.li@rock-chips.com>

--- librkvpu/rk_vepu/vp8_enc/rk_vp8encapi.h
+++ librkvpu/rk_vepu/vp8_enc/rk_vp8encapi.h
@@ -5,52 +5,40 @@
 
 #ifndef _RK_VP8ENCAPI_H_
 #define _RK_VP8ENCAPI_H_
-
+#include "rk_venc.h"
 #include "vp8encapi.h"
 #include "rk_vepu_interface.h"
 
 enum VP8E_PRIVATE_DATA_TYPE {
-  VP8E_PRIVATE_DATA_TYPE_END,
-  VP8E_PRIVATE_DATA_TYPE_FRAMETAG,
-  VP8E_PRIVATE_DATA_TYPE_FRAMEHEADER,
-  VP8E_PRIVATE_DATA_TYPE_CABAC,
-  VP8E_PRIVATE_DATA_TYPE_SEGMAP,
-  VP8E_PRIVATE_DATA_TYPE_REG,
-  VP8E_PRIVATE_DATA_TYPE_PROBCNT
-};
-
-struct rk_vp8_encoder;
-
-struct rk_venc_ops {
-  int (*init)(struct rk_vp8_encoder *enc, struct rk_vepu_init_param *enc_parms);
-  int (*before_encode)(struct rk_vp8_encoder *enc);
-  int (*after_encode)(struct rk_vp8_encoder *enc, uint32_t outputStreamSize);
-  void (*deinit)(struct rk_vp8_encoder *enc);
-  int (*updatepriv)(struct rk_vp8_encoder *enc, void *config, uint32_t cfglen);
-  void (*updateparameter)(struct rk_vp8_encoder *enc, struct rk_vepu_runtime_param *param);
+	VP8E_PRIVATE_DATA_TYPE_END,
+	VP8E_PRIVATE_DATA_TYPE_FRAMETAG,
+	VP8E_PRIVATE_DATA_TYPE_FRAMEHEADER,
+	VP8E_PRIVATE_DATA_TYPE_CABAC,
+	VP8E_PRIVATE_DATA_TYPE_SEGMAP,
+	VP8E_PRIVATE_DATA_TYPE_REG,
+	VP8E_PRIVATE_DATA_TYPE_PROBCNT
 };
 
 #define NUM_CTRLS 3
 struct rk_vp8_encoder {
-  struct rk_venc_ops *ops;
+	struct rk_venc *parent;
 
-  EncoderParameters cmdl;
-  VP8EncInst encoder;
-  VP8EncIn encIn;
-  int intraPeriodCnt;
-  int codedFrameCnt;
-  int src_img_size;
-  int next;
-  uint8_t *priv_data;
-  uint32_t priv_offset; /* offset of current private data */
-  uint32_t hdr_idx;
-  bool first_frame;
-  uint32_t rk_ctrl_ids[NUM_CTRLS];
-  void *rk_payloads[NUM_CTRLS];
-  uint32_t rk_payload_sizes[NUM_CTRLS];
+	EncoderParameters cmdl;
+	VP8EncInst encoder;
+	VP8EncIn encIn;
+	int intraPeriodCnt;
+	int codedFrameCnt;
+	int src_img_size;
+	int next;
+	uint8_t *priv_data;
+	uint32_t priv_offset;	/* offset of current private data */
+	uint32_t hdr_idx;
+	bool first_frame;
+	uint32_t rk_ctrl_ids[NUM_CTRLS];
+	void *rk_payloads[NUM_CTRLS];
+	uint32_t rk_payload_sizes[NUM_CTRLS];
 };
 
-struct rk_vp8_encoder* rk_vp8_encoder_alloc_ctx(void);
-void rk_vp8_encoder_free_ctx(struct rk_vp8_encoder *enc);
-#endif /* _RK_VP8ENCAPI_H_ */
-
+struct rk_venc *rk_vp8_encoder_alloc_ctx(void);
+void rk_vp8_encoder_free_ctx(struct rk_venc *enc);
+#endif				/* _RK_VP8ENCAPI_H_ */
